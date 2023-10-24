17d70a34dddd603011f4ac01dd94432ad4a61e9c
Keshav22wno <keshav22wno@gmail.com>
2023-10-24 18:33:32 +0530

    Name input output active

---
+++ hello.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+def main():
+    main_v1()
+
+def main_v1():
+    print("we designed first main function)
+    name = input("Enter your name: ")
+    print("Your name is:",name)
+
+if __name__ == "__main__":
+    main()
+
